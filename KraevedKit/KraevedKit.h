40c5d52ba04c9fb256f43a58c322adcb5f46d487
Амелькин Владимир <amelkin@vbankcenter.ru>
2022-12-28 18:46:48 +0300

    Initial Commit

---
+++ KraevedKit/KraevedKit.h
@@ -0,0 +1,18 @@
+//
+//  KraevedKit.h
+//  KraevedKit
+//
+//  Created by Владимир Амелькин on 28.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KraevedKit.
+FOUNDATION_EXPORT double KraevedKitVersionNumber;
+
+//! Project version string for KraevedKit.
+FOUNDATION_EXPORT const unsigned char KraevedKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KraevedKit/PublicHeader.h>
+
+
