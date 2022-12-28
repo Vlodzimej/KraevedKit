d38bebb3c3ba7882c5916a789709e5ab7c98d397
Амелькин Владимир <amelkin@vbankcenter.ru>
2022-12-28 20:09:00 +0300

    Changed podspec

---
+++ KraevedKit.podspec
@@ -0,0 +1,25 @@
+Pod::Spec.new do |spec|
+
+  spec.name         = "KraevedKit"
+  spec.version      = "0.0.1"
+  spec.summary      = "Kit for Kraeved app"
+
+  spec.description  = <<-DESC
+Learning project for GPB-iOS. Represents a square view with name
+                   DESC
+
+  spec.homepage     = "https://github.com/Vlodzimej/KraevedKit"
+  spec.license      = { :type => "MIT", :file => "LICENSE" }
+  spec.author       = { "vlodzimej" => "amel-07@mail.ru" }
+
+  spec.ios.deployment_target = "12.0"
+  spec.swift_version = "5.7.1"
+
+  spec.source        = { :git => "https://github.com/Vlodzimej/KraevedKit.git", :tag => "#{spec.version}" }
+  spec.source_files  = "KraevedKit/**/*.{h,m,swift}"
+
+  spec.dependency 'SnapKit'
+  spec.dependency 'OtusHomework'
+
+  spec.public_header_files = "KraevedKit/**/*.{h}"
+end
--- KraevedKit/KTextField.swift
+++ KraevedKit/KTextField.swift
@@ -1,8 +1,46 @@
 //
 //  KTextField.swift
-//  KraevedKit
+//  Kraeved
 //
 //  Created by Владимир Амелькин on 28.12.2022.
 //
 
-import Foundation
+import UIKit
+
+class KTextField: UITextField {
+    private let bottomLineColor: UIColor
+    private var bottomLineLayer: CALayer?
+    
+    var isBottomLineHidden: Bool = false {
+        didSet {
+            bottomLineLayer?.isHidden = isBottomLineHidden
+        }
+    }
+    
+    init(bottomLineColor: UIColor = UIColor.gray) {
+        self.bottomLineColor = bottomLineColor
+        super.init(frame: .zero)
+        borderStyle = .none
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if bottomLineLayer == nil {
+            bottomLineLayer = CALayer()
+        } else {
+            bottomLineLayer?.removeFromSuperlayer()
+        }
+        
+        guard let bottomLineLayer = bottomLineLayer else { return }
+        
+        bottomLineLayer.frame = CGRect(x: 0, y: frame.height - 2, width: frame.width, height: 2)
+        bottomLineLayer.backgroundColor = bottomLineColor.cgColor
+        
+        layer.addSublayer(bottomLineLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
